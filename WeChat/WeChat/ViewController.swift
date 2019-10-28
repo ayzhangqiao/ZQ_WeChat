3e0813453e2919e240a400c05bf98f5ea1ed8414
zhangqq <zhangqq@weipaitang.com>
2019-10-28 15:12:52 +0800

    set up project

---
+++ WeChat/WeChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeChat
+//
+//  Created by zhangqiaoqiao on 2019/10/28.
+//  Copyright © 2019 张乔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
